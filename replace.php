191faaf8f72049a594a62710ef7cc5aab39f383d
Axx972 <123100991+Axx972@users.noreply.github.com>
2025-08-29 11:00:03 +0200

    modification et ajout de la fonction

--- replace.php
+++ replace.php
@@ -1,5 +1,9 @@
 <?php
-$text = "Bonjour le Monde !";
-$replace = str_replace(' ','', $text); // str_replace() remplace les espaces par rien
-echo strlen($replace)." caractères";// strlen() compte le nombre de caractères
-?>
+function nbChar() {
+    $text = "Bonjour le Monde !";// On crée une variable avec du texte
+    $replace = str_replace(' ', '', $text);// On remplace les espaces par rien
+    return strlen($replace); // On retourne le nombre de caractères sans les espaces
+}
+
+//On affiche le résultat :
+echo nbChar();
