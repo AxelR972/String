e204a75b083dcae4224f482111e5e4f40317b446
Axx972 <123100991+Axx972@users.noreply.github.com>
2025-08-29 12:29:07 +0200

    fonction qui inverse l'ordre des mots dans une phrase

--- occurrence.php
+++ occurrence.php
@@ -1,7 +1,15 @@
 <?php
 function occuurence() {
     $mot = "Je mange une pomme";
-    $lettre = "e";
+    $lettre = "a";
     return substr_count($mot, $lettre);
 }
 echo occuurence();
+
+function reverse() {
+    $frenchPhrase = "Je mange une pomme";
+    $words = explode(" ", $frenchPhrase);
+    $reversedWords = array_reverse($words);
+    return implode(" ", $reversedWords);
+}
+echo reverse();
